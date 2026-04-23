f47891e02a2b7eb31d66c2408c0d7bb806f81998
iprohit <iplrohit10@gmail.com>
2026-04-23 14:09:48 +0530

    build

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,21 +8,35 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Rohit Ippalpalli </span>
-            from <span className="purple"> Solapur, Maharashtra, India.</span>
-            <br /> I Have Completed My B.Tech in Civil Engineering From A. G. Patil Institute of Technology, Solapur
+            Hi Everyone, I am{" "}
+            <span className="purple">Rohit Ippalpalli </span>
+            from{" "}
+            <span className="purple">Solapur, Maharashtra, India.</span>
             <br />
-            Additionally, I Have Undergone a Software Development Course From QSpiders.
+            <br />
+            I am a Software Developer with experience in building and deploying 
+            scalable applications using modern cloud and DevOps practices.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            I have worked as a Software Developer at SPW Healthcare Innovations, 
+            where I contributed to developing and deploying applications using 
+            AWS, CI/CD pipelines, Docker, and Jenkins.
+            <br />
+            <br />
+            I also have prior experience as an Associate Support Engineer at Quick Heal, 
+            which helped me build a strong foundation in troubleshooting and system reliability.
+            <br />
+            <br />
+            My core skills include AWS (EC2, S3), CI/CD, Docker, Jenkins, Linux, 
+            Spring Boot, React, Python, MySQL, and MongoDB.
           </p>
+
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Building cloud & DevOps projects
             </li>
             <li className="about-activity">
-              <ImPointRight /> Netflix & Chill !!
+              <ImPointRight /> Exploring AWS & automation tools
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -30,7 +44,7 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Strive to build scalable systems that make a real impact!"
           </p>
           <footer className="blockquote-footer">Rohit</footer>
         </blockquote>
